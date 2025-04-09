0b7b9d5207f325720f8124f4296ef5226d7d14ef
dwiajisobarna <dwiajisobarna1@gmail.com>
2025-04-09 13:50:31 +0700

    Initial Commit

---
+++ NewsFetcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewsFetcher
+//
+//  Created by Dwi Aji Sobarna on 09/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewsFetcher/NewsFetcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFetcherApp.swift
+//  NewsFetcher
+//
+//  Created by Dwi Aji Sobarna on 09/04/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsFetcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
